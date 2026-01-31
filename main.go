b21eccea7360e3758a4b1aa284efbb2b6a6cd082
protuzok <turykruslanz@gmail.com>
2026-01-31 18:12:39 +0000

    work with errors and fix all warns

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"context"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
@@ -15,34 +16,79 @@ import (
 const dbURL = "postgres://user:password@localhost:5432/mydb"
 
 func main() {
+	logFile, err := startupLogger()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Logger start up")
+
+	defer func() {
+		err = logFile.Close()
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "Error closing log file: %v\n", err)
+		}
+	}()
+
 	ctx := context.Background()
 
-	pool, done := startTable(ctx, dbURL)
-	if !done {
-		return
+	pool, err := startupTable(ctx, dbURL)
+	if err != nil {
+		fmt.Println("Помилка старту таблиці бази даних: ", err)
+		log.Println(err)
+		os.Exit(1)
 	}
 	defer pool.Close()
 
-	for true {
+	for {
 		printOptions()
 
-		opt := takeOption()
+		opt, err := takeOption()
+		if err != nil {
+			fmt.Println("Помилка вводу:", err)
+			continue
+		}
 
 		switch opt {
 		case 0:
 			return
 		case 1:
-			addField(pool, ctx)
+			err = addField(pool, ctx)
+			if err != nil {
+				log.Println(err)
+				os.Exit(1)
+			}
 		case 2:
-			printTable(pool, ctx)
+			err = printTable(pool, ctx)
+			if err != nil {
+				log.Println(err)
+				os.Exit(1)
+			}
+
 		case 3:
-			deleteFields(pool, ctx)
+			err = deleteFields(pool, ctx)
+			if err != nil {
+				log.Println(err)
+				os.Exit(1)
+			}
 
 		default:
 			fmt.Println("Спробуйте ще раз")
 			continue
 		}
 	}
+}
+
+func startupLogger() (*os.File, error) {
+	logFile, err := os.OpenFile("app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, fmt.Errorf("impossible to open log file: %w", err)
+	}
+
+	log.SetOutput(logFile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+
+	return logFile, nil
 }
 
 func printOptions() {
@@ -54,17 +100,19 @@ func printOptions() {
 	fmt.Println("3) Видалити поле(-я) з таблиці за id")
 }
 
-func takeOption() int {
+func takeOption() (int, error) {
 	input := takeInput("Оберіть опцію:")
 
-	option, _ := strconv.ParseInt(input, 10, 64)
-	// todo обробка помилок
+	option, err := strconv.ParseInt(input, 10, 64)
+	if err != nil {
+		return -1, err
+	}
 
-	return int(option)
+	return int(option), nil
 }
 
 func takeInput(instruction string) (input string) {
-	for true {
+	for {
 		fmt.Println(instruction)
 		fmt.Printf("-> ")
 		scanner := bufio.NewScanner(os.Stdin)
@@ -88,21 +136,21 @@ func takeInput(instruction string) (input string) {
 
 // -----------------------------------------------------------------
 
-func startTable(ctx context.Context, dbURL string) (p *pgxpool.Pool, done bool) {
-	p, err := pgxpool.New(ctx, dbURL)
+func startupTable(ctx context.Context, dbURL string) (p *pgxpool.Pool, err error) {
+	p, err = pgxpool.New(ctx, dbURL)
 	if err != nil {
-		return nil, false
+		return nil, fmt.Errorf("error start up data base table: %w", err)
 	}
 
 	createTableSQL := "CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, name TEXT NOT NULL, surname TEXT NOT NULL, year INT);"
 	_, err = p.Exec(ctx, createTableSQL)
 	if err != nil {
-		return nil, false
+		return nil, fmt.Errorf("error start up data base table: %w", err)
 	}
-	return p, true
+	return p, nil
 }
 
-func addField(pool *pgxpool.Pool, ctx context.Context) {
+func addField(pool *pgxpool.Pool, ctx context.Context) error {
 	input := takeInput("Введіть значення полів запису:")
 
 	var fields = strings.Fields(input)
@@ -114,11 +162,15 @@ func addField(pool *pgxpool.Pool, ctx context.Context) {
 
 	insertRecordSQL := "INSERT INTO users(name, surname, year) VALUES ($1, $2, $3)"
 
-	// todo всі ці функції будуть повинні повертати err, а не просто void
-	pool.Exec(ctx, insertRecordSQL, sqlArgs...)
+	_, err := pool.Exec(ctx, insertRecordSQL, sqlArgs...)
+	if err != nil {
+		return fmt.Errorf("impossible to add field: %w", err)
+	}
+
+	return nil
 }
 
-func printTable(pool *pgxpool.Pool, ctx context.Context) {
+func printTable(pool *pgxpool.Pool, ctx context.Context) error {
 	fmt.Println("Таблиця користувачів:")
 	fmt.Printf("%-5s %-20s %-20s %-5s \n", "id", "name", "surname", "year")
 
@@ -129,42 +181,32 @@ func printTable(pool *pgxpool.Pool, ctx context.Context) {
 
 	for rows.Next() {
 		var u User
-		rows.Scan(&u.id, &u.name, &u.surname, &u.year)
+		err := rows.Scan(&u.id, &u.name, &u.surname, &u.year)
+		if err != nil {
+			return fmt.Errorf("impossible to print table: %w", err)
+		}
 
 		fmt.Printf("%-5d %-20s %-20s %-5d \n", u.id, u.name, u.surname, u.year)
 	}
+
+	return nil
 }
 
-func deleteFields(pool *pgxpool.Pool, ctx context.Context) {
+func deleteFields(pool *pgxpool.Pool, ctx context.Context) error {
 	input := takeInput("Введіть список id для видалення:")
 
-	ids := strings.Fields(input)
-
-	if len(ids) == 0 {
-		fmt.Println("Ви не ввели жодного ID")
-		return
+	fields := strings.Fields(input)
+	if len(fields) == 0 {
+		fmt.Println("Не введено жодного ID.")
+		return nil
 	}
 
-	var placeholders strings.Builder
-	placeholders.WriteString("(")
+	deleteRecordsSQL := "DELETE FROM users WHERE id = ANY($1::int[])"
 
-	for i := 1; i <= len(ids); i++ {
-		var s string
-		if i != len(ids) {
-			s = fmt.Sprintf("$%d, ", i)
-		} else {
-			s = fmt.Sprintf("$%d)", i)
-		}
-
-		placeholders.WriteString(s)
+	_, err := pool.Exec(ctx, deleteRecordsSQL, fields)
+	if err != nil {
+		return fmt.Errorf("impossible to delete fields: %w", err)
 	}
 
-	var sqlIds = make([]any, len(ids))
-	for i, v := range ids {
-		sqlIds[i] = v
-	}
-
-	deleteRecordsSQL := "DELETE FROM users WHERE id IN " + placeholders.String()
-
-	pool.Exec(ctx, deleteRecordsSQL, sqlIds...)
+	return nil
 }
