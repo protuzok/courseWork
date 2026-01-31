d08ff6309909edac4c2ad7b4a58c2937fa293656
protuzok <turykruslanz@gmail.com>
2026-01-31 08:03:48 +0000

    refactor input-output system

--- main.go
+++ main.go
@@ -5,17 +5,19 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+// todo треба буде змінити структуру таблиці та методи для роботи з нею. І назву бази даних
 const dbURL = "postgres://user:password@localhost:5432/mydb"
 
 func main() {
 	ctx := context.Background()
 
-	pool, done := startTable(ctx)
+	pool, done := startTable(ctx, dbURL)
 	if !done {
 		return
 	}
@@ -24,16 +26,15 @@ func main() {
 	for true {
 		printOptions()
 
-		var option int
-		takeOptions(&option)
+		opt := takeOption()
 
-		switch option {
+		switch opt {
 		case 0:
 			return
 		case 1:
 			addField(pool, ctx)
 		case 2:
-			readTable(pool, ctx)
+			printTable(pool, ctx)
 		case 3:
 			deleteFields(pool, ctx)
 
@@ -44,47 +45,50 @@ func main() {
 	}
 }
 
-func deleteFields(pool *pgxpool.Pool, ctx context.Context) {
-	fmt.Println("Введіть список id для видалення:")
-	fmt.Printf("-> ")
-	scanner := bufio.NewScanner(os.Stdin)
-
-	if scanner.Scan() {
-		input := scanner.Text()
-
-		ids := strings.Fields(input)
-
-		if len(ids) == 0 {
-			fmt.Println("Ви не ввели жодного ID")
-			return
-		}
-
-		var placeholders strings.Builder
-		placeholders.WriteString("(")
-
-		for i := 1; i <= len(ids); i++ {
-			var s string
-			if i != len(ids) {
-				s = fmt.Sprintf("$%d, ", i)
-			} else {
-				s = fmt.Sprintf("$%d)", i)
-			}
-
-			placeholders.WriteString(s)
-		}
-
-		var sqlIds = make([]any, len(ids))
-		for i, v := range ids {
-			sqlIds[i] = v
-		}
-
-		deleteRecordsSQL := "DELETE FROM users WHERE id IN " + placeholders.String()
-
-		pool.Exec(ctx, deleteRecordsSQL, sqlIds...)
-	}
+func printOptions() {
+	fmt.Println("")
+	fmt.Println("============================")
+	fmt.Println("0) Вийти з програми")
+	fmt.Println("1) Додати поле в таблицю")
+	fmt.Println("2) Вивести всю таблицю")
+	fmt.Println("3) Видалити поле(-я) з таблиці за id")
 }
 
-func startTable(ctx context.Context) (p *pgxpool.Pool, done bool) {
+func takeOption() int {
+	input := takeInput("Оберіть опцію:")
+
+	option, _ := strconv.ParseInt(input, 10, 64)
+	// todo обробка помилок
+
+	return int(option)
+}
+
+func takeInput(instruction string) (input string) {
+	for true {
+		fmt.Println(instruction)
+		fmt.Printf("-> ")
+		scanner := bufio.NewScanner(os.Stdin)
+
+		if scanner.Scan() {
+			input = scanner.Text()
+
+			if input == "" {
+				fmt.Println("Пустий рядок. Спробуйте ще раз")
+				fmt.Println("")
+
+				continue
+			}
+		}
+
+		break
+	}
+
+	return input
+}
+
+// -----------------------------------------------------------------
+
+func startTable(ctx context.Context, dbURL string) (p *pgxpool.Pool, done bool) {
 	p, err := pgxpool.New(ctx, dbURL)
 	if err != nil {
 		return nil, false
@@ -98,35 +102,10 @@ func startTable(ctx context.Context) (p *pgxpool.Pool, done bool) {
 	return p, true
 }
 
-func printOptions() {
-	fmt.Println("")
-	fmt.Println("============================")
-	fmt.Println("Оберіть опцію:")
-	fmt.Println("0) Вийти з програми")
-	fmt.Println("1) Додати поле в таблицю")
-	fmt.Println("2) Вивести всю таблицю")
-	fmt.Println("3) Видалити поле(-я) з таблиці за id")
-}
-
-func takeOptions(option *int) {
-	fmt.Printf("-> ")
-	fmt.Scan(option)
-	fmt.Println("")
-}
-
-// todo написати функції для вводу (стрічка для виведення і сам ввід). Таке чат робив для ЧМ пам'ятаю
 func addField(pool *pgxpool.Pool, ctx context.Context) {
-	fmt.Println("Введіть значення полів запису:")
-	fmt.Printf("-> ")
-	scanner := bufio.NewScanner(os.Stdin)
+	input := takeInput("Введіть значення полів запису:")
 
-	// todo десь тут треба додати перевірку, чи користувач взагалі шось ввів (deleteFields)
-	var fields []string
-	if scanner.Scan() {
-		input := scanner.Text()
-
-		fields = strings.Fields(input)
-	}
+	var fields = strings.Fields(input)
 
 	sqlArgs := make([]any, len(fields))
 	for i, v := range fields {
@@ -139,7 +118,7 @@ func addField(pool *pgxpool.Pool, ctx context.Context) {
 	pool.Exec(ctx, insertRecordSQL, sqlArgs...)
 }
 
-func readTable(pool *pgxpool.Pool, ctx context.Context) {
+func printTable(pool *pgxpool.Pool, ctx context.Context) {
 	fmt.Println("Таблиця користувачів:")
 	fmt.Printf("%-5s %-20s %-20s %-5s \n", "id", "name", "surname", "year")
 
@@ -154,4 +133,38 @@ func readTable(pool *pgxpool.Pool, ctx context.Context) {
 
 		fmt.Printf("%-5d %-20s %-20s %-5d \n", u.id, u.name, u.surname, u.year)
 	}
+}
+
+func deleteFields(pool *pgxpool.Pool, ctx context.Context) {
+	input := takeInput("Введіть список id для видалення:")
+
+	ids := strings.Fields(input)
+
+	if len(ids) == 0 {
+		fmt.Println("Ви не ввели жодного ID")
+		return
+	}
+
+	var placeholders strings.Builder
+	placeholders.WriteString("(")
+
+	for i := 1; i <= len(ids); i++ {
+		var s string
+		if i != len(ids) {
+			s = fmt.Sprintf("$%d, ", i)
+		} else {
+			s = fmt.Sprintf("$%d)", i)
+		}
+
+		placeholders.WriteString(s)
+	}
+
+	var sqlIds = make([]any, len(ids))
+	for i, v := range ids {
+		sqlIds[i] = v
+	}
+
+	deleteRecordsSQL := "DELETE FROM users WHERE id IN " + placeholders.String()
+
+	pool.Exec(ctx, deleteRecordsSQL, sqlIds...)
 }
